02fa3e8eeaf4971672d74425b3c6e0790810206e
Kelvins <75732727+Kelvins97@users.noreply.github.com>
2025-05-05 18:35:53 +0300

    Update supabase.js

--- server/supabase.js
+++ server/supabase.js
@@ -1,6 +1,6 @@
-const { createClient } = require('@supabase/supabase-js');
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
-module.exports = createClient(supabaseUrl, supabaseKey);
+export default createClient(supabaseUrl, supabaseKey);
