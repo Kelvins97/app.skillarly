6b82449b9ac86c4903237eb9ead2c05d96e791a3
Kelvins <75732727+Kelvins97@users.noreply.github.com>
2025-05-13 13:52:50 +0300

    Update linkedin.js

--- server/auth/linkedin.js
+++ server/auth/linkedin.js
@@ -21,7 +21,7 @@ const configureLinkedInStrategy = () => {
     clientID: process.env.LINKEDIN_CLIENT_ID,
     clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
     callbackURL: process.env.LINKEDIN_CALLBACK_URL,
-    scope: ['openid', 'profile', 'email'],
+    scope: ['openid', 'profile', 'w_member_social', 'email'],
     state: true,
     passReqToCallback: true
   }, (req, accessToken, refreshToken, profile, done) => {
