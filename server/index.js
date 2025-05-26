a9cd2625e59f32ce11154f6ba0dafc0f1971d924
Kelvins <75732727+Kelvins97@users.noreply.github.com>
2025-05-26 17:25:03 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,11 +30,11 @@ dotenv.config();
 
 // 2. Initialize Core Services
 const app = express();
-app.use(helmet());
+//app.use(helmet());
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
+//app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 const stripe = stripePackage(process.env.STRIPE_SECRET_KEY);
 const { Pool } = pg;
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
