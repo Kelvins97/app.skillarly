0df791f616361297a57352530771e925f8f31d2a
Kelvins <75732727+Kelvins97@users.noreply.github.com>
2025-05-27 18:50:26 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -44,10 +44,8 @@ const redisClient = createClient({ url: process.env.REDIS_URL });
 
 // Serve uploaded resumes statically -- Resume Routes
 app.use(express.json());
-app.use('/resume', authenticate, resumeRoutes);
 app.use(resumeRoutes); 
-app.use('/resumes', express.static(path.join(__dirname, 'resumes')));
-
+app.use('/api/resume', resumeRoutes);
 
 // 3. Enhanced CORS Configuration
 const corsOptions = {
