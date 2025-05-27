b676c6f346d395008d83463c8231671ab72d190d
Kelvins <75732727+Kelvins97@users.noreply.github.com>
2025-05-27 19:43:48 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,6 @@ import authRoutes from './authRoutes.js'
 import devSeedRoute from './dev-seed.js';
 import resumeRoutes from './routes/resume.js';
 import cron from 'node-cron';
-import helmet from 'helmet';
 import './cronJob.js';
 
 // 1. Environment Configuration
@@ -30,11 +29,9 @@ dotenv.config();
 
 // 2. Initialize Core Services
 const app = express();
-app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true }));
-//app.use(morgan('dev'));
+
 const stripe = stripePackage(process.env.STRIPE_SECRET_KEY);
 const { Pool } = pg;
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
